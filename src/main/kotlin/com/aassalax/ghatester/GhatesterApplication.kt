d39fc372ec2076f3cbc86a0d3e109c0018fd9039
aassalax <aassali.dev@gmail.com>
2025-07-07 19:27:14 +0200

    first commit

---
+++ src/main/kotlin/com/aassalax/ghatester/GhatesterApplication.kt
@@ -0,0 +1,11 @@
+package com.aassalax.ghatester
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GhatesterApplication
+
+fun main(args: Array<String>) {
+	runApplication<GhatesterApplication>(*args)
+}
---
+++ src/test/kotlin/com/aassalax/ghatester/GhatesterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aassalax.ghatester
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GhatesterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
